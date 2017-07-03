729a1ba8ad402b05bdb0680f1fb8a28f54376f0f
Nathan Brown <nkabrown@gmail.com>
2017-07-03 15:26:52 -0400

    Modularize and bundle

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,39 +1,8 @@
-/*
- * Frequency Distribution Tables – How frequently is data distributed at each measure?
- *
- * The first column contains the measure ordered from highest to lowest, the second column contains the number of cases of that measure.
- * Use the symbol N to represent the total number of cases in a group.
- */
+'use strict';
+import { FrequencyDistTable } from './freq-dist-table';
+const d3 = require('d3');
 
-class FrequencyDistTable {
-  constructor(el, d, m, c, a) {
-    this.mount = el;
-    this.data = d;
-    this.measure = m;
-    this.caption = c;
-    this.attribution = a;
-  }
-
-  init() {
-    d3.select('figcaption')
-       .text(this.caption);
-
-    d3.select('thead')
-       .html(d => `<tr><th>${this.measure}</th><th>&#402;</th><th>${this.measure}</th><th>&#402;</th><th>${this.measure}</th><th>&#402;</th></tr>`);
-
-    d3.select(this.mount)
-       .selectAll('.row')
-       .data(this.data)
-     .enter().append('tr')
-       .attr('class', 'row')
-       .html(d => `<td>${d[0]}</td><td>${d[1]}</td><td>${d[2]}</td><td>${d[3]}</td><td>${d[4]}</td><td>${d[5]}</td>`);
-
-    d3.select('small')
-       .text(this.attribution);
-  }
-}
-
-d3.csv('../data/scores.csv', (error, data) => {
+d3.csv('../../data/scores.csv', (error, data) => {
   if (error) throw error;
 
   const maxValue = d3.max(data, d => +d.score);
@@ -45,7 +14,7 @@ d3.csv('../data/scores.csv', (error, data) => {
   }
 
   // for every score in dataset increment map by one
-  for (d of data) {
+  for (let d of data) {
     const key = +d.score;
     if (scores.has(key)) {
       let val = scores.get(key);
@@ -73,20 +42,19 @@ d3.csv('../data/scores.csv', (error, data) => {
     rows.push(row_data);
   }
 
-
   d3.text('src/modal/frequencyTable.html', str => {
      d3.select('.container').append('div').attr('class', 'freq-table').html(str);
 
      new FrequencyDistTable('tbody', rows, 'SCORE', 'Table 2.1   /   Simple Frequency Distribution of Anxiety Scores for 100 Colege Students', 'Ahana, E. Y. A study on the reliability and internal consistency of a manifest anxiety scale. M.A. thesis, Northwestern Univeristy, 1952.').init();
 
-  d3.select(`tbody tr:nth-child(${lastColumnLength + 1}) td:nth-child(5)`)
-      .attr('data-symbol', 'N')
-      .append('span')
-      .text('N=');
+    d3.select(`tbody tr:nth-child(${lastColumnLength + 1}) td:nth-child(5)`)
+        .attr('data-symbol', 'N')
+        .append('span')
+        .text('N=');
 
-  d3.select(`tbody tr:nth-child(${lastColumnLength + 1}) td:nth-child(6)`)
-      .append('span')
-      .attr('class', 'lineover')
-      .text(`${d3.sum(scores, d => d[1])}`);
+    d3.select(`tbody tr:nth-child(${lastColumnLength + 1}) td:nth-child(6)`)
+        .append('span')
+        .attr('class', 'lineover')
+        .text(`${d3.sum(scores, d => d[1])}`);
   });
 });
---
+++ src/js/freq-dist-table.js
@@ -0,0 +1,30 @@
+'use strict';
+const d3 = require('d3');
+
+export class FrequencyDistTable {
+  constructor(el, d, m, c, a) {
+    this.mount = el;
+    this.data = d;
+    this.measure = m;
+    this.caption = c;
+    this.attribution = a;
+  }
+
+  init() {
+    d3.select('figcaption')
+       .text(this.caption);
+
+    d3.select('thead')
+       .html(d => `<tr><th>${this.measure}</th><th>&#402;</th><th>${this.measure}</th><th>&#402;</th><th>${this.measure}</th><th>&#402;</th></tr>`);
+
+    d3.select(this.mount)
+       .selectAll('.row')
+       .data(this.data)
+     .enter().append('tr')
+       .attr('class', 'row')
+       .html(d => `<td>${d[0]}</td><td>${d[1]}</td><td>${d[2]}</td><td>${d[3]}</td><td>${d[4]}</td><td>${d[5]}</td>`);
+
+    d3.select('small')
+       .text(this.attribution);
+  }
+}
